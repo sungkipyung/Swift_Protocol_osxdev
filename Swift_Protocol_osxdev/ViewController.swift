6e96221b002784ac259fb4c01f0f53152fe41ae2
othead h <hothead@hothead-MacBook-Pro.local>
2016-10-22 22:54:54 +0900

    Initial Commit

---
+++ Swift_Protocol_osxdev/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift_Protocol_osxdev
+//
+//  Created by hothead on 2016. 10. 22..
+//  Copyright © 2016년 hothead. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
